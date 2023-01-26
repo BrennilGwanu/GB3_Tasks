eb24fc28f74187d23a9dd470cbcdb18ac52e6a86
Snake <per.mortis.non.lates@gmail.com>
2023-01-26 23:45:51 +0500

    comleted

---
+++ Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int x = 1;
+while (x<=n)
+{
+    Console.Write($"{Math.Pow(x,3)}");
+     if (x!=n) Console.Write(", ");
+    x++;
+}
