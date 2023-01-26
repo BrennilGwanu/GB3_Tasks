aad33eab81f10c56c64b7d83d691f0263523fc5d
Snake <per.mortis.non.lates@gmail.com>
2023-01-26 23:27:19 +0500

    comlited

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number / 10000 == number % 10)
+{
+    x = number / 1000 % 10;
+    y = number / 100 % 10;
+    if (x == y)
+    {
+        Console.Write("да");
+    }
+    else Console.Write("нет");
+}
+else
+Console.Write("нет");
